b1a0be7d754b6eba1574deba029bd4a4d62a6266
itzzzjordan <143443387+itzzzjordan@users.noreply.github.com>
2023-09-25 14:36:41 -0400

    first upload

---
+++ P2LAB2_GiraudJordan.py
@@ -0,0 +1,16 @@
+# Jordan Giraud
+# 9/25/23
+# Displays floating-point notation
+
+
+num1 = float(input("Enter a float number: "))
+num2 = float(input("Enter a float number: "))
+num3 = float(input("Enter a float number: "))
+num4 = float(input("Enter a float number: "))
+
+product = num1*num2*num3*num4
+average = (num1+num2+num3+num4)/4
+
+print(f"{product:.0f}\t{average:.0f}")
+print(f"{product:.3f}\t{average:.3f}")
+
